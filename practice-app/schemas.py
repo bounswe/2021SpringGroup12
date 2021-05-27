5a1cb8a03083ae284cf6894dd7d7f3d4c86e4324
veyis <vturgut68@gmail.com>
2021-05-27 15:19:00 +0300

    post /books is done, added database support

--- practice-app/schemas.py
+++ practice-app/schemas.py
@@ -2,16 +2,6 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
-class CreateBook(BaseModel):
-    book_title: str
-    book_author: str
-    url: Optional[str] = ""
-    publication_dt: Optional[str] = ""
-    summary: Optional[str] = ""
-    uuid: Optional[str] = ""
-    uri: Optional[str] = ""
-    isbn13: Optional[List[str]]= ""
-
 class Book(BaseModel):
     book_title: str
     book_author: str
@@ -23,6 +13,9 @@ class Book(BaseModel):
     isbn13: List[str]
     
 class BookResponse(BaseModel):
-    copyright: Optional[str]
+    copyright: Optional[str] = ""
     num_results: int
     books: List[dict]
+
+class ErrorResponse(BaseModel):
+    message: Optional[str] =""
