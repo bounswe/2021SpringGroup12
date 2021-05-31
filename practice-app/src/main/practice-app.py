31db8ffe281c17d150d8e161860e6a7848edf0b3
veyis <vturgut68@gmail.com>
2021-05-31 21:01:56 +0300

    organized and structured code

--- practice-app/src/main/practice-app.py
+++ practice-app/src/main/practice-app.py
@@ -7,8 +7,7 @@ from flask import Flask, jsonify, Response, request
 import requests
 from db import schemas, mapper
 import sqlite3
-from helpers import issue_helper
-from helpers.issue_helper import ALL_ISSUES
+from helpers import issue_helper, books_helper
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -27,83 +26,30 @@ def get_tasks():
 
 @app.route('/books/', methods=['GET'])
 def get_books():
-    # if name parameter is not supplied, return 400
-    if "name" not in request.args:
-        return Response("Please provide an author name!", status=400)
-    # now we are sure name parameter is supplied, request books of this author from NYTimes API.
-    author_name = request.args.get("name").title().replace(" ", "+")
-    r = requests.get(
-        "https://api.nytimes.com/svc/books/v3/reviews.json?author={}&api-key=Ug3r4KRJxcd69bNw5i89CWxArqiGlrGB".format(
-            author_name))
-    r = r.json()
-    # now we have books of this author in r.
-    books = [mapper.book_mapper(s) for s in r["results"]]
+    # validate parameters
+    x = books_helper.validate_input(request.args)
+    if x is not None:
+        return x
+    # fetch results from 3rd party api
+    books = books_helper.call_nytimes(request.args.get("name"))
+    if type(books) != list:
+        return books
+    # now we have books of this author in books variable.
     # Let's store this book in database for further references.
-    con = sqlite3.connect("./sqlfiles/practice-app.db")
-    cur = con.cursor()
-    for book in books:
-        try:
-            cur.execute(
-                "INSERT INTO Books(book_title, book_author, url, publication_dt, summary, uuid, uri) VALUES (?,?,?,?,?,?,?)",
-                (book.book_title, book.book_author, book.url, book.publication_dt, book.summary, book.uuid, book.uri))
-            cur.execute("SELECT book_id FROM Books WHERE book_title = ? AND book_author = ?",
-                        (book.book_title, book.book_author))
-            book_id = cur.fetchone()[0]
-            for isbn in book.isbn13:
-                cur.execute("INSERT INTO BookISBNs(book_id, isbn) VALUES (?,?)", (book_id, isbn))
-        except:
-            # do nothing upon failure, this is not a critical process
-            continue
-    con.commit()
-    con.close()
-    # if user wants to remove copyright content, make it empty string.
-    if request.args.get("no_copyright") == "1":
-        r["copyright"] = ""
-
+    books_helper.add_books_from_nytimes(books)
     ## don't return all books, return just as much as user wants
-    if request.args.get("max_results") is not None and int(r["num_results"]) > int(request.args.get("max_results")):
-        max_results = int(request.args.get("max_results"))
-        r["num_results"] = max_results
-        books = books[:max_results]
-
-    return schemas.BookResponse(copyright=r["copyright"], num_results=r["num_results"], books=books).__dict__
+    books = books_helper.get_n(books,request.args.get("max_results"))
+    return books if type(books) != list else schemas.BookResponse(num_results=len(books), books=books).__dict__
 
 
 @app.route('/books/', methods=['POST'])
 def create_book():
-    book_fields = request.get_json()
-
-    # make sure that necessary information are given
-    if "book_title" not in book_fields.keys():
-        return Response("Please provide the name of the book!", status=400)
-    if "book_author" not in book_fields.keys():
-        return Response("Please provide the name of the author!", status=400)
-    try:
-        Book = mapper.book_mapper(book_fields)
-    except Exception as err:
-        return Response(str(err), status=409)
-    # connect to Database
-    con = sqlite3.connect("./sqlfiles/practice-app.db")
-    cur = con.cursor()
+    book = books_helper.validate_body(request.get_json())
+    if type(book) is not schemas.Book:
+        return book
     # try to insert book to DB, return forbidden upon failure
-    try:
-        cur.execute(
-            "INSERT INTO Books(book_title, book_author, url, publication_dt, summary, uuid, uri) VALUES (?,?,?,?,?,?,?)",
-            (Book.book_title, Book.book_author, Book.url, Book.publication_dt, Book.summary, Book.uuid, Book.uri))
-    except Exception as err:
-        return Response(str(err), status=403)
-    # since isbn's can be a list. Insert those to a seperate table.
-    cur.execute("select * from Books")
-    if Book.isbn13 is not []:
-        data = cur.execute("SELECT book_id FROM Books WHERE book_title = ? AND book_author = ?",
-                           (Book.book_title, Book.book_author))
-        book_id = data.fetchone()[0]
-        for isbn in list(set(Book.isbn13)):
-            cur.execute("INSERT INTO BookISBNs(book_id, isbn) VALUES (?,?)", (book_id, isbn))
-
-    con.commit()
-    con.close()
-    return Response("Book added succesfully!", status=200)
+    db_response = books_helper.add_book_from_user(book)
+    return db_response if db_response is not None else Response("Book added succesfully!", status=200)
 
 
 @app.route('/download_issues', methods=['GET'])
