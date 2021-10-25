ea1a43ee9414d3d19f7c5f2342617767cefb41f6
Veyis Turgut <vturgut68@gmail.com>
2021-10-26 01:34:50 +0300

    empty gradle spring boot project

---
+++ application/backend/src/main/java/cmpe451/group12/beabee/BeabeeApplication.java
@@ -0,0 +1,13 @@
+package cmpe451.group12.beabee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeabeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeabeeApplication.class, args);
+	}
+
+}
