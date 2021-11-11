c2cc9f5fed3b7d9e5433f66592e1feee52475acf
ihsangkcl <ihsangokcul@gmail.com>
2021-11-12 02:27:19 +0300

    feature(dev/task-routine-question-reflection): implemented task and taskedit layouts. #258

---
+++ application/android/app/src/main/java/com/group12/beabee/TaskCardViewAdapter.java
@@ -0,0 +1,46 @@
+package com.group12.beabee;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskCardViewAdapter extends RecyclerView.Adapter<TaskCardViewAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_task_cardview, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return 10;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView description;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.tv_title);
+            description = itemView.findViewById(R.id.tv_description);
+        }
+
+        public void OnDataBind(){
+
+        }
+    }
+}
