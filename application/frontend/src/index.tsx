c942c1a646b2481d33b6023dec1a5befae2f7440
omeryilmazrengen <oyilmaz@rengen.io>
2021-11-16 14:21:07 +0300

    Ip adress of backend is updated.

--- application/frontend/src/index.tsx
+++ application/frontend/src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
 
-axios.defaults.baseURL = "http://3.141.24.166:8085/"
+axios.defaults.baseURL = "http://3.144.201.198:8085/"
 console.log(axios.defaults.baseURL)
 
 ReactDOM.render(
