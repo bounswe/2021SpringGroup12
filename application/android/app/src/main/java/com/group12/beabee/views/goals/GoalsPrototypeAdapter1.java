6319ec160e7a6aa18ce279ca5926d14f9adbe264
Berk Yamanoğlu <berkyamanoglu@hazelcast.com>
2021-12-31 19:36:14 +0300

    remove unnecessary viewholder

--- application/android/app/src/main/java/com/group12/beabee/views/goals/GoalsPrototypeAdapter1.java
+++ application/android/app/src/main/java/com/group12/beabee/views/goals/GoalsPrototypeAdapter1.java
@@ -33,11 +33,6 @@ public class GoalsPrototypeAdapter1 extends RecyclerView.Adapter<GoalsPrototypeA
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        if(viewType==0){
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_goal_list_marketplace, parent, false);
-            return new ViewHolder(view);
-        }
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_goal_list_marketplace, parent, false);
         return new ViewHolder(view);
     }
@@ -45,14 +40,6 @@ public class GoalsPrototypeAdapter1 extends RecyclerView.Adapter<GoalsPrototypeA
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.BindData(goalShortList.get(position));
-        //holder.getItemViewType()
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        // Just as an example, return 0 or 2 depending on position
-        // Note that unlike in ListView adapters, types don't have to be contiguous
-        return position % 2 * 2;
     }
 
     @Override
@@ -85,34 +72,6 @@ public class GoalsPrototypeAdapter1 extends RecyclerView.Adapter<GoalsPrototypeA
             itemParent.setOnClickListener(v -> onItemClickedListener.OnGoalClicked(goalShort.id));
             tvUsername.setText(goalShort.username);
             //downloadnum.setText(goalShort.download_count);
-        }
-    }
-    class ViewHolder2 extends RecyclerView.ViewHolder {
-
-        @BindView(R.id.tv_goal_title)
-        TextView tvTitle;
-        @BindView(R.id.tv_description)
-        TextView tvDescription;
-        @BindView(R.id.item_parent)
-        View itemParent;
-        @BindView(R.id.tv_username)
-        TextView tvUsername;
-        @BindView(R.id.download_number)
-        TextView downloadnum;
-
-
-        public ViewHolder2(@NonNull View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-        }
-
-        public void BindData(GoalShort goalShort) {
-            tvTitle.setText(goalShort.title);
-            tvDescription.setText(goalShort.description);
-            itemParent.setOnClickListener(v -> onItemClickedListener.OnGoalClicked(goalShort.id));
-            tvUsername.setText(goalShort.username);
-            //downloadnum.setText(goalShort.download_count);
-
         }
     }
 }
