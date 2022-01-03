c737fbb5aeb85036119d53d6d3a5db094d6dc3a3
Burakcan Yeşil <brkcnkzm@gmail.com>
2022-01-03 23:08:03 +0300

    fix(backend): fixed mapper missing @Component annotation #513

--- application/backend/src/main/java/cmpe451/group12/beabee/goalspace/mapper/goals/GroupGoalPostMapper.java
+++ application/backend/src/main/java/cmpe451/group12/beabee/goalspace/mapper/goals/GroupGoalPostMapper.java
@@ -1,14 +1,13 @@
 package cmpe451.group12.beabee.goalspace.mapper.goals;
 
-import cmpe451.group12.beabee.goalspace.dto.goals.GoalDTOShort;
 import cmpe451.group12.beabee.goalspace.dto.goals.GroupGoalPostDTO;
-import cmpe451.group12.beabee.goalspace.model.goals.Goal;
 import cmpe451.group12.beabee.goalspace.model.goals.GroupGoal;
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class GroupGoalPostMapper {
     //GroupGoalPostDTO mapToDto(GroupGoal groupGoal);
 
