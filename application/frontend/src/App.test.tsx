5cb5d3cdc35fbe8d3acabf19b5f94dcef1adbe06
Batuhan <63856065+batuhanovski@users.noreply.github.com>
2022-01-04 12:40:02 +0300

    unit tests
    unit testss

--- application/frontend/src/App.test.tsx
+++ application/frontend/src/App.test.tsx
@@ -5,7 +5,9 @@ import App from './App';
 import NavBar from './components/NavBar'
 import Sidebar from './components/Sidebar';
 import { Menu } from "antd";
-
+import { SignOutButton } from "./components/SignOutButton";
+import { LoginForm } from "./components/LoginForm";
+import { Login } from "./pages/Login";
 
 test('Check navbar component is displayed', async () => {
   const wrapper = mount(<App/>)
@@ -21,3 +23,44 @@ test('sidebar has no element when there is no logged in user', async () => {
   expect(wrapper.find(Sidebar).contains("Dashboard")).toBeFalsy()
 });
 
+//batu
+
+test('Navigation bar has not Feed when there is no logged in user', async () => {
+  const wrapper = mount(<App/>)
+  await waitFor(()=>{})
+  expect(wrapper.find(NavBar).contains("Feed")).toBeFalsy()
+});
+
+test('Navigation bar has Login when there is no logged in user', async () => {
+  const wrapper = mount(<App/>)
+  await waitFor(()=>{})
+  expect(wrapper.find(NavBar).contains("Login")).toBeTruthy()
+});
+
+test('Navigation bar has Register when there is no logged in user', async () => {
+  const wrapper = mount(<App/>)
+  await waitFor(()=>{})
+  expect(wrapper.find(NavBar).contains("Register")).toBeTruthy()
+});
+
+
+/*
+test('Check signout button is displayed', async () => {
+  const wrapper = mount(<NavBar user="batu"/>)
+  await waitFor(()=>{})
+  expect(wrapper.find(Home)).toBeTruthy()
+});
+
+
+test('Check username text is displayed on login page', async () => {
+  const wrapper = mount(<Login/>)
+  await waitFor(()=>{})
+  expect(wrapper.find(LoginForm).contains("Username")).toBeTruthy()
+});
+
+test('Check login text is displayed on login page', async () => {
+  const wrapper = mount(<Login/>)
+  await waitFor(()=>{})
+  expect(wrapper.find(LoginForm).contains("Password")).toBeTruthy()
+});
+*/
